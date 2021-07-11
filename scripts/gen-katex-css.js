17c18a42d5c90e723e06f881a0e46d6c0131096a
ddu <ddu6@protonmail.com>
2021-07-12 05:15:40 +0800

    update

--- scripts/gen-katex-css.js
+++ scripts/gen-katex-css.js
@@ -1,4 +1,4 @@
 const fs=require('fs')
 const path=require('path')
 const url=require('@ddu6/url-tools')
-fs.appendFileSync(path.join(__dirname,'../css/0.katex.css'),url.fixURLInCSS(fs.readFileSync(path.join(__dirname,'../node_modules/katex/dist/katex.css'),{encoding:'utf8'}),'https://cdn.jsdelivr.net/npm/katex/dist/'))
+fs.writeFileSync(path.join(__dirname,'../css/0.katex.css'),url.fixURLInCSS(fs.readFileSync(path.join(__dirname,'../node_modules/katex/dist/katex.css'),{encoding:'utf8'}),'https://cdn.jsdelivr.net/npm/katex/dist/'))
