946b9b312cd5945152c4abfebfe2cbfc9d33a0dc
DDU <ddu6@protonmail.com>
2021-11-06 23:09:01 +0800

    update

--- dist/mod.d.ts
+++ dist/mod.d.ts
@@ -1,3 +1,4 @@
 export * as tagToUnitCompiler from './ucs';
+export * from './common';
 export declare const css: string;
 export declare const headCSS = "@import \"https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css\";";
--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,5 @@
 export * as tagToUnitCompiler from './ucs'
+export * from './common'
 import {css as hlCSS} from 'sthl'
 import {all,head} from './lib/css'
 export const css=hlCSS+all
