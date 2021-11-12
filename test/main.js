141e826e6bd7dad5c4afea7a2892a28567fde716
DDU <ddu6@protonmail.com>
2021-11-12 22:57:51 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
-import {css,headCSS,tagToUnitCompiler} from '../dist/mod'
-import {compile} from '@ddu6/stc'
-import {Shell} from '@ddu6/stui'
+import {css,headCSS,tagToUnitCompiler} from '../mod.js'
+import {compile} from 'https://cdn.jsdelivr.net/npm/@ddu6/stc/mod.js'
+import {Shell} from 'https://cdn.jsdelivr.net/npm/@ddu6/stui/mod.js'
 const headStyle=document.createElement('style')
 const customStyle=document.createElement('style')
 headStyle.textContent=headCSS
