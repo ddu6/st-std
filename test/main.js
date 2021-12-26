592e445a5b8f27ac1c200cfafa115724214ec347
DDU <ddu6@protonmail.com>
2021-12-26 17:38:42 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,5 +1,5 @@
 import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.8.1/mod.js'
-import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.3.1/mod.js'
+import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.3.2/mod.js'
 import {css,headCSS,tagToUnitCompiler} from '../mod.js'
 init({css})
 const headStyle=document.createElement('style')
