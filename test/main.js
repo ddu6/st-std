59e51717eb6a3d74bdc902a86de38d65cee2b43c
DDU <ddu6@protonmail.com>
2022-01-12 23:38:11 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,11 +1,10 @@
 import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.1/mod.js'
-import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.10.0/mod.js'
+import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/mod.js'
 import * as tagToUnitCompiler from '../ucs.js'
 init()
 const stdStyle = document.createElement('style')
 const style = document.createElement('style')
-stdStyle.textContent = `@import url(https://cdn.jsdelivr.net/gh/st-org/stui@0.10.0/main.css);
-@import url(https://cdn.jsdelivr.net/gh/st-org/stui@0.10.0/struct.css);
+stdStyle.textContent = `@import url(https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/main.css);
 @import url("../main.css");`
 document.head.append(stdStyle)
 document.head.append(style)
