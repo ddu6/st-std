f45c932cb43cdee3f3d2a0d87c09d6fb4c0a539f
DDU <ddu6@protonmail.com>
2022-01-13 02:39:32 +0800

    update

--- test/main.js
+++ test/main.js
@@ -2,10 +2,7 @@ import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.1/mod.js'
 import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/mod.js'
 import * as tagToUnitCompiler from '../ucs.js'
 init()
-const stdStyle = document.createElement('style')
 const style = document.createElement('style')
-stdStyle.textContent = `@import url("../main.css");`
-document.head.append(stdStyle)
 document.head.append(style)
 const result = await compile(await (await fetch('./main.stdn')).text(), location.href, {
     builtInTagToUnitCompiler: tagToUnitCompiler,
