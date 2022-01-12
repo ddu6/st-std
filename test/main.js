9eebd281b048d0bddbe2dd61b4c9e77c16831c45
DDU <ddu6@protonmail.com>
2022-01-12 17:25:15 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.0/mod.js'
+import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.1/mod.js'
 import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.9.1/mod.js'
 import {css, tagToUnitCompiler} from '../mod.js'
 init({css})
