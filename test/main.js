f88c64456a1f73d11a82a3535be0a24f4527c539
DDU <ddu6@protonmail.com>
2022-01-19 19:41:35 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,5 +1,5 @@
 import {compile} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.2/mod.js'
-import {createASStruct, init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.3/mod.js'
+import {createASStruct, init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.4/mod.js'
 import * as tagToUnitCompiler from '../ucs.js'
 init()
 const style = document.createElement('style')
