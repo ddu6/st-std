620b2b92190ec7cddc1ac124864973ffc1274b90
DDU <ddu6@protonmail.com>
2022-06-06 22:59:58 +0800

    update

--- src/import.ts
+++ src/import.ts
@@ -5,10 +5,6 @@ export const {getMod} = createImporter<{
     katex: typeof katex
     sthl: typeof sthl
 }>({
-/*
-    katex: 'https://cdn.jsdelivr.net/npm/katex@0.15.6/dist/katex.min.mjs',
-    sthl: 'https://cdn.jsdelivr.net/gh/st-org/sthl@0.13.3/mod.js'
-*/
     katex: 'https://cdn.bootcdn.net/ajax/libs/KaTeX/0.15.6/katex.mjs',
     sthl: '../sthl@0.13.3/mod.js'
 })
