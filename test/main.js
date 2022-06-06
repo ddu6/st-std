062d7ff52c924ff6b36f1c8566bf774ce3b2fc88
DDU <ddu6@protonmail.com>
2022-06-06 10:21:34 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-import {compileURLs} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.0/mod.js'
+import {compileURLs} from 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.1/mod.js'
 import {createASStruct, init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.12/mod.js'
 import * as tagToUnitCompiler from '../ucs.js'
 init()
