bf698cda7c1ea2aa85b3f0bcd62bb5f813d2cf45
DDU <ddu6@protonmail.com>
2022-12-19 22:17:33 +0800

    update

--- dist/import.js
+++ dist/import.js
@@ -1,5 +1,5 @@
 import { createImporter } from '@ddu6/importer';
 export const { getMod } = createImporter({
-    katex: 'https://cdn.jsdelivr.net/npm/katex@0.15.6/dist/katex.min.mjs',
+    katex: 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.mjs',
     sthl: 'https://cdn.jsdelivr.net/gh/st-org/sthl@0.13.3/mod.js'
 });
--- src/import.ts
+++ src/import.ts
@@ -5,6 +5,6 @@ export const {getMod} = createImporter<{
     katex: typeof katex
     sthl: typeof sthl
 }>({
-    katex: 'https://cdn.jsdelivr.net/npm/katex@0.15.6/dist/katex.min.mjs',
+    katex: 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.mjs',
     sthl: 'https://cdn.jsdelivr.net/gh/st-org/sthl@0.13.3/mod.js'
 })
